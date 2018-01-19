a3bfb5a2cec81ade64325a3db53ac03c21399366
Juan José Hernández Alonso <anselm82@gmail.com>
2018-01-19 19:32:16 +0100

    Raúl - Fix conference list show

--- CoBra/Conference/ConferenceTableViewController.swift
+++ CoBra/Conference/ConferenceTableViewController.swift
@@ -174,6 +174,41 @@ class ConferenceTableViewController: UITableViewController {
     }
 }
 
+extension ConferenceTableViewController : NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .delete:
+            let indexSet = IndexSet(arrayLiteral: sectionIndex)
+            tableView.deleteSections(indexSet, with: .fade)
+        case .insert:
+            let indexSet = IndexSet(arrayLiteral: sectionIndex)
+            tableView.insertSections(indexSet, with: .fade)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .update: tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .insert: tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete: tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .move: tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+}
+
 extension ConferenceTableViewController {
     
     @objc func downloadConferencesList(_ control :UIRefreshControl?) {
